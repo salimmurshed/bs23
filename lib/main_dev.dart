9c94723d3e2fb3a8aab516b46c562943000f9b18
Salim Vitec <122268027+Salim-Vitec@users.noreply.github.com>
2024-03-31 15:08:17 +0600

    fix: android version compile issue

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,8 @@
-import 'app/app_config.dart';
-import 'main.dart';
-
-void main() async {
-  ConstantEnvironment.setEnvironment(Environment.dev);
-  await Future.delayed(Duration.zero);
-  mainDelegate();
-}
+// import 'app/app_config.dart';
+// import 'main.dart';
+//
+// void main() async {
+//   ConstantEnvironment.setEnvironment(Environment.dev);
+//   await Future.delayed(Duration.zero);
+//   mainDelegate();
+// }
